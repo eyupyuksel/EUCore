6879ad6fd2274d69fa8623fb20cff44370c8757e
Eyüp Yüksel <eyupyuksel@outlook.com>
2020-06-29 23:10:54 +0300

    runtimeInitialzer added

---
+++ EUCore/Configuration/RuntimeInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DapperExtensions.Sql;
+using Dommel;
+using EUCore.Repositories.Dapper.Dommel;
+
+namespace EUCore.Configuration
+{
+    public class RuntimeInitializer
+    {
+        
+        public RuntimeInitializer SetDialect<TDialect>() where TDialect : SqlDialectBase, new()
+        {
+            DapperExtensions.DapperExtensions.SqlDialect = new TDialect();
+            return this;
+        }
+        public RuntimeInitializer DefaultMapper(Type type)
+        {
+            DapperExtensions.DapperExtensions.DefaultMapper = type;
+            DommelMapper.SetTableNameResolver(new TableNameResolver());
+            DommelMapper.SetKeyPropertyResolver(new KeyPropertyResolver());
+            DommelMapper.SetColumnNameResolver(new ColumnNameResolver());
+            return this;
+        }
+    }
+}
